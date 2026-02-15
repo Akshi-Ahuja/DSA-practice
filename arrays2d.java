5ded04931ebdaae7c3c495f15a2f155aecb9e615
Akshi Ahuja <akshiahuja2204@gmail.com>
2026-02-15 15:12:16 +0530

    2dArrays topic

---
+++ arrays2d.java
@@ -0,0 +1,100 @@
+import java.util.*;
+
+public class arrays2d {
+
+    public static void searchMatrix(int matrix[][], int key) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == key) {
+                    System.out.println("Key found at: " + i + ", " + j);
+                }
+            }
+        }
+    }
+
+    public static void matrixRange(int matrix[][]) {
+        int largest = Integer.MIN_VALUE;
+        int smallest = Integer.MAX_VALUE;
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] > largest) {
+                    largest = matrix[i][j];
+                }
+                if (matrix[i][j] < smallest) {
+                    smallest = matrix[i][j];
+                }
+            }
+        }
+
+        System.out.println("Matrix range starts from " + smallest + " to " + largest);
+    }
+
+    public static void spiralMatrix(int matrix[][]) {
+        int startRow = 0;
+        int startCol = 0;
+        int endRow = matrix.length - 1;
+        int endCol = matrix[0].length - 1;
+
+        while (startRow <= endCol && startCol <= endCol) {
+            // top
+            for (int j = startCol; j <= endCol; j++) {
+                System.out.print(matrix[startRow][j] + " ");
+            }
+
+            // right
+            for (int i = startRow + 1; i <= endRow; i++) {
+                System.out.print(matrix[i][endCol] + " ");
+            }
+
+            // bottom
+            for (int j = endCol - 1; j >= startCol; j--) {
+                if (startCol == endCol) {
+                    break;
+                }
+                System.out.print(matrix[endRow][j] + " ");
+            }
+
+            // left
+            for (int i = endRow - 1; i >= startCol + 1; i--) {
+                if (startRow == endRow) {
+                    break;
+                }
+                System.out.print(matrix[i][startCol] + " ");
+            }
+
+            // variable updates
+            startRow++;
+            startCol++;
+            endCol--;
+            endRow--;
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int matrix[][] = new int[4][4];
+
+        // input matrix elements
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        // print matrix elements
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        searchMatrix(matrix, 4);
+        matrixRange(matrix);
+        spiralMatrix(matrix);
+    }
+}
