925f9f541d6f6a3ed740fa908051ffeaf8c6449b
Akshi Ahuja <akshiahuja2204@gmail.com>
2026-03-02 17:18:32 +0530

    OOPS concepts completed

--- OOPS.java
+++ OOPS.java
@@ -1,5 +1,10 @@
 public class OOPS {
     public static void main(String[] args) {
+        Complex c1 = new Complex(1, -2);
+        Complex c2 = new Complex(2, -1);
+        c1.printComplex();
+
+        (Complex.add(c1, c2)).printComplex();
 
     }
 }
@@ -35,3 +40,39 @@ class Pen {
 // percentage = (double) (phy + chm + maths) / 3;
 // }
 // }
+
+// Assignment Questions:
+
+class Complex {
+    int real;
+    int imag;
+
+    public Complex(int r, int i) {
+        real = r;
+        imag = i;
+    }
+
+    public static Complex add(Complex a, Complex b) {
+        return new Complex((a.real + b.real), (a.imag + b.imag));
+    }
+
+    public static Complex diff(Complex a, Complex b) {
+        return new Complex((a.real - b.real), (a.imag - b.imag));
+    }
+
+    public static Complex multiply(Complex a, Complex b) {
+        return new Complex(
+                ((a.real * b.real) - (a.imag * b.imag)),
+                ((a.real * b.real) + (a.imag * b.imag)));
+    }
+
+    public void printComplex() {
+        if (real == 0 && imag != 0) {
+            System.out.println(imag + "i");
+        } else if (imag == 0 && real != 0) {
+            System.out.println(real);
+        } else {
+            System.out.println(real + " + " + imag + "i");
+        }
+    }
+}
