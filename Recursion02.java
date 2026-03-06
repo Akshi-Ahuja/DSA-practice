7e743345b9610b2098934a3d1ab4af47ba8b7f4c
Akshi Ahuja <akshiahuja2204@gmail.com>
2026-03-06 19:19:52 +0530

    Recursion 02 completed

--- Recursion02.java
+++ Recursion02.java
@@ -37,10 +37,46 @@ public class Recursion02 {
 
     }
 
+    public static int friendsPairing(int n) {
+        // base case
+        if (n == 1 || n == 2)
+            return n;
+
+        // kaam - choice
+
+        // single
+        int fnm1 = friendsPairing(n - 1);
+
+        // pairing
+        int fnm2 = friendsPairing(n - 2);
+        int pairWays = (n - 1) * fnm2;
+
+        int totWays = fnm1 + pairWays;
+        return totWays;
+    }
+
+    public static void printBinStr(int n, int lastPlace, String str) {
+        // base case
+        if (n == 0) {
+            System.out.println(str);
+            return;
+        }
+
+        // kaam
+        printBinStr(n - 1, 0, str + "0");
+        if (lastPlace == 0) {
+            printBinStr(n - 1, 1, str + "1");
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println(tillingProblem(5));
 
         String str = "aappnaccollegd";
         removeDup(str, 0, new StringBuilder(""), new boolean[26]);
+
+        System.out.println(friendsPairing(4));
+
+        printBinStr(3, 0, "");
     }
 }
