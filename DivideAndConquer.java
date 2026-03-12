5d0aa4e065364185e3e0658a14234dd61eba8894
Akshi Ahuja <akshiahuja2204@gmail.com>
2026-03-12 17:09:59 +0530

    Divide and Conquer completed

--- DivideAndConquer.java
+++ DivideAndConquer.java
@@ -52,9 +52,132 @@ public class DivideAndConquer {
         merge(arr, si, mid, ei);
     }
 
+    // Quick Sort
+
+    public static void quickSort(int arr[], int si, int ei) {
+        // base case
+        if (si >= ei)
+            return;
+
+        // kaam
+
+        // last element - pivot
+        int pIdx = partition(arr, si, ei);
+
+        quickSort(arr, si, pIdx - 1); // left
+        quickSort(arr, pIdx + 1, ei); // right
+    }
+
+    public static int partition(int arr[], int si, int ei) {
+        int pivot = arr[ei];
+        int i = si - 1;
+
+        for (int j = si; j < ei; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                // swap
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        // place the pivot at its right place as well
+        i++;
+        // swap for pivot
+        int temp = pivot;
+        arr[ei] = arr[i];
+        arr[i] = temp;
+
+        return i;
+    }
+
+    // Search Target in Rotated Sorted Array - Recursion
+
+    public static int search(int arr[], int target, int si, int ei) {
+        if (si > ei) {
+            return -1;
+        }
+
+        // kaam
+        int mid = si + (ei - si) / 2;
+
+        if (arr[mid] == target) {
+            return mid;
+        }
+
+        // mid lies on L1
+        if (arr[si] <= arr[mid]) {
+            // case a: left of mid
+            if (arr[si] <= target && target <= arr[mid]) {
+                return search(arr, target, si, mid - 1);
+            }
+            // case b: right to mid
+            else {
+                return search(arr, target, mid + 1, ei);
+            }
+        }
+
+        // mid lies on L2
+        else {
+            // case c: target right to mid
+            if (arr[mid] <= target && target <= arr[ei]) {
+                return search(arr, target, mid + 1, ei);
+            }
+            // case d: left to mid
+            else {
+                return search(arr, target, si, mid - 1);
+            }
+        }
+    }
+
+    // Search Target in Rotated Sorted Array - Iterations
+
+    public static int searchWithIterations(int arr[], int target) {
+        int si = 0;
+        int ei = arr.length;
+        while (si <= ei) {
+            int mid = si + (ei - si) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            }
+
+            if (arr[si] <= arr[mid]) {
+                if (arr[si] <= target && target <= arr[mid]) {
+                    ei = mid - 1;
+                } else {
+                    si = mid + 1;
+                }
+            }
+
+            else {
+                if (arr[mid] <= target && target <= arr[ei]) {
+                    si = mid + 1;
+                } else {
+                    ei = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
     public static void main(String[] args) {
-        int arr[] = { 6, 3, 9, 5, 2, 8, -2 };
-        mergeSort(arr, 0, arr.length - 1);
-        printArray(arr);
+        // MERGE SORT
+        // int arr[] = { 6, 3, 9, 5, 2, 8, -2 };
+        // mergeSort(arr, 0, arr.length - 1);
+        // printArray(arr);
+
+        // QUICK SORT
+        // int arr02[] = { 6, 5, 8, 2, 1, 3 };
+        // quickSort(arr02, 0, arr02.length - 1);
+        // printArray(arr02);
+
+        // SEARCH TARGET IN ROTATED SORTED ARRAY
+        int arr03[] = { 4, 5, 6, 7, 0, 1, 2 };
+        int target = 6;
+        // int tIdx = search(arr03, target, 0, arr03.length - 1);
+        int tIdx = searchWithIterations(arr03, target);
+        System.out.println(tIdx);
     }
 }
